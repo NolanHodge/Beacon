a2d2204fd0af43ef40deda86efd171b500e4638f
Jason <tehblackboy@gmail.com>
2016-09-29 14:38:19 -0400

    first real commit

---
+++ Beacon/app/src/main/java/com/comp3004/beacon/GUI/ArrowActivity.java
@@ -0,0 +1,14 @@
+package com.comp3004.beacon.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.comp3004.beacon.R;
+
+public class ArrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrow);
+    }
+}
