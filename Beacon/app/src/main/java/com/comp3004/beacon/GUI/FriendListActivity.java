c1c33c67056fc151ba5fd9d521e9eb33c132d2ed
nolanhodge <nolanhodge@github.com>
2016-10-03 22:45:37 -0400

    New Activites for all

---
+++ Beacon/app/src/main/java/com/comp3004/beacon/GUI/FriendListActivity.java
@@ -0,0 +1,15 @@
+package com.comp3004.beacon.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.comp3004.beacon.R;
+
+public class FriendListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+    }
+}
