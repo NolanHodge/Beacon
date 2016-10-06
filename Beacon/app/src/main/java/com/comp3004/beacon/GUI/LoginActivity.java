15f6a64b10bcc110d9a699ba9b592872ff124086
Jason <tehblackboy@gmail.com>
2016-10-06 19:26:00 -0400

    bunch of updates including fixing arrow, location, maps, reducing app size

--- Beacon/app/src/main/java/com/comp3004/beacon/GUI/LoginActivity.java
+++ Beacon/app/src/main/java/com/comp3004/beacon/GUI/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.comp3004.beacon.GUI;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
@@ -7,7 +9,22 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
+
+import com.comp3004.beacon.FbStuff;
+import com.comp3004.beacon.MapsActivity;
 import com.comp3004.beacon.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
 import android.os.Build;
 import android.os.Bundle;
@@ -28,14 +45,44 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-/**
- * A login screen that offers login via google
- * TODO Facebook
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+    CallbackManager callbackManager;
+
+    private FacebookCallback<LoginResult> mFacebookCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(final LoginResult loginResult) {
+
+            AccessToken accessToken = loginResult.getAccessToken();
+            handleFacebookAccessToken(accessToken);
+        }
+
+
+        @Override
+        public void onCancel() {
+            Log.d("Facebook", "Cancelled");
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            Log.d("Facebook", "Error " + e);
+        }
+    };
+
 
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
@@ -55,6 +102,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
+
+//        startActivity(new Intent(this, FbStuff.class));
         setContentView(R.layout.activity_login);
 
         //Fields
@@ -77,6 +127,54 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         // Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+        setupTokenTracker();
+        setupProfileTracker();
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
+
+        if (AccessToken.getCurrentAccessToken() != null && Profile.getCurrentProfile() != null) {
+
+            final SharedPreferences settings = getSharedPreferences("MyPrefsFile", 0);
+            if(settings.getBoolean("autocheckin", true)){
+                //startService(new Intent(this, MyService.class));
+            }
+
+            //startService(new Intent(this, MsgService.class));
+            startActivity(new Intent(this, MainActivity.class));
+
+            finish();
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, mFacebookCallback);
+        loginButton.setReadPermissions("public_profile, user_friends");
+
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
 
     }
     @Override
@@ -116,6 +214,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 Log.e(TAG, "Google Sign In failed.");
             }
         }
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
@@ -208,7 +308,65 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
     }
     public void openArrow(View v){
-        startActivity(new Intent(this, ArrowActivity.class));
+        startActivity(new Intent(this, MapsActivity.class));
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("", "handleFacebookAccessToken:" + token);
+
+        final AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("", "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
+    private void setupTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.d("User", "" + currentAccessToken);
+            }
+        };
+    }
+
+    private void setupProfileTracker() {
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(com.facebook.Profile profile, com.facebook.Profile profile1) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.stopTracking();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
 
